43d61500c0764f6151f7e74265b5962b55ac8193
Ashank <145696141+Ashankshukla25@users.noreply.github.com>
2026-02-16 11:06:20 +0530

    fiest commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+class Hello{
+    public static void main(String[] args) {
+        String name = "Ashank";
+        System.out.println("First code");
+        System.out.println("Hello from "+name);
+    }
+}
